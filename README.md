ad5f66b0418f2ec8818e0136dd99f789441ba3ea
Sietse Ludger Geertsema <sietse.ludger.geertsema@turnn.nl>
2021-08-28 10:01:30 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# zyxel-t50
+# Zyxel T50
+A small wrapper for the Zyxel T50 modem.
+
+It can retrieve basic status of the modem and a list of connected devices.
+This is used for a [device tracking integration](https://github.com/home-assistant/core/tree/dev/homeassistant/components/zyxelt50) of [Home Assistant](https://www.home-assistant.io/).
+
+# Simple example
+```python
+import asyncio
+import json
+from src.zyxelt50.modem import ZyxelT50Modem
+
+
+async def my_function():
+    router = ZyxelT50Modem('#YOUR ADMIN PASSWORD#')
+    await router.connect()
+
+    status = await router.get_connected_devices()
+    print(json.dumps(status, indent=4))
+
+asyncio.run(my_function())
+```
---
+++ src/zyxelt50/modem.py
@@ -0,0 +1,135 @@
+""" The Zyxel T50 modem """
+""" I used these as a starting point: https://github.com/ThomasRinsma/vmg8825scripts """
+
+import base64
+import json
+import logging
+
+from Crypto.Cipher import PKCS1_v1_5
+from Crypto.PublicKey import RSA
+import requests
+
+from .helpers import decrypt_response, encrypt_request
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class ZyxelT50Modem:
+
+    def __init__(self,
+                 password=None,
+                 host='192.168.1.1',
+                 username='admin') -> None:
+        self.url = host
+        self.user = username
+        self.password = password
+
+        self.r = requests.Session()
+        self.r.trust_env = False  # ignore proxy settings
+
+        # we define the AesKey ourselves
+        self.aes_key = b'\x42' * 32
+        self.enc_aes_key = None
+        self.sessionkey = None
+
+        self._model = None
+        self._sw_version = None
+        self._unique_id = None
+
+    async def connect(self) -> None:
+        """Set up a Zyxel modem."""
+        self.enc_aes_key = await self.__get_aes_key()
+
+        try:
+            await self.__login()
+        except CannotConnect:
+            _LOGGER.error("Failed to connect to modem")
+
+        status = await self.get_device_status()
+
+        device_info = status["DeviceInfo"]
+        if self._unique_id is None:
+            self._unique_id = device_info["SerialNumber"]
+
+        self._model = device_info["ModelName"]
+        self._sw_version = device_info["SoftwareVersion"]
+
+    async def __get_aes_key(self):
+        # ONCE
+        # get pub key
+        response = self.r.get(f"http://{self.url}/getRSAPublickKey")
+        pubkey_str = response.json()['RSAPublicKey']
+
+        # Encrypt the aes key with RSA pubkey of the device
+        pubkey = RSA.import_key(pubkey_str)
+        cipher_rsa = PKCS1_v1_5.new(pubkey)
+        return cipher_rsa.encrypt(base64.b64encode(self.aes_key))
+
+    async def __login(self):
+        login_data = {
+            "Input_Account": self.user,
+            "Input_Passwd": base64.b64encode(self.password.encode('ascii')).decode('ascii'),
+            "RememberPassword": 0,
+            "SHA512_password": False
+        }
+
+        enc_request = encrypt_request(self.aes_key, login_data)
+        enc_request['key'] = base64.b64encode(self.enc_aes_key).decode('ascii')
+        response = self.r.post(f"http://{self.url}/UserLogin", json.dumps(enc_request))
+        decrypted_response = decrypt_response(self.aes_key, response.json())
+
+        if decrypted_response is not None:
+            response = json.loads(decrypted_response)
+
+            self.sessionkey = response['sessionkey']
+            return 'result' in response and response['result'] == 'ZCFG_SUCCESS'
+
+        _LOGGER.error("Failed to decrypt response")
+        raise CannotConnect
+
+    async def logout(self):
+        response = self.r.post(f"http://{self.url}/cgi-bin/UserLogout?sessionKey={self.sessionkey}")
+        response = response.json()
+
+        if 'result' in response and response['result'] == 'ZCFG_SUCCESS':
+            return True
+        else:
+            return False
+
+    async def __get_device_info(self, oid):
+        response = self.r.get(f"http://{self.url}/cgi-bin/DAL?oid={oid}")
+        decrypted_response = decrypt_response(self.aes_key, response.json())
+        if decrypted_response is not None:
+            json_string = decrypted_response.decode('utf8').replace("'", '"')
+            json_data = json.loads(json_string)
+            return json_data['Object'][0]
+
+        _LOGGER.error("Failed to get device status")
+        return None
+
+    async def get_device_status(self):
+        result = await self.__get_device_info("cardpage_status")
+        if result is not None:
+            return result
+
+        _LOGGER.error("Failed to get device status")
+        return None
+
+    async def get_connected_devices(self):
+        result = await self.__get_device_info("lanhosts")
+        if result is not None:
+            devices = {}
+            for device in result['lanhosts']:
+                devices[device['PhysAddress']] = {
+                    "hostName": device['HostName'],
+                    "physAddress": device['PhysAddress'],
+                    "ipAddress": device['IPAddress'],
+                }
+            return devices
+
+        _LOGGER.error("Failed to connected devices")
+        return []
+
+
+class CannotConnect(Exception):
+    """Error to indicate we cannot connect."""
